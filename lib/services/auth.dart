8a60244192848cacc0361e44b6f450dc4261530b
mhmoud <mhmoudsoliman41@outlook.com>
2021-01-03 13:25:07 +0200

    auth Commit

---
+++ lib/services/auth.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in with email and password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return null;
+    } on FirebaseAuthException catch (e){
+      return e.message;
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+  // register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return null;
+    } on FirebaseAuthException catch (e){
+      return e.message;
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+}
